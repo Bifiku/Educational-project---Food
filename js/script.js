c2b8c7dbcee307336ddbeab05d60e7c39bfbb00e
Andrey work <example@gmail.com>
2023-03-06 17:19:35 +0300

    Сделал переключение между табами без анимации

---
+++ js/script.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabcontainer');
+
+    function HideTabsContent() {
+        tabsContent.forEach((item) => {
+            item.style.display = 'none';
+        });
+        
+        tabs.forEach((item) => {
+            if (item.classList.contains('tabheader__item_active')){
+                item.classList.remove('tabheader__item_active');
+            }
+        });
+    }
+
+    function ShowTabsContent(i){
+        tabsContent[i].style.display = 'block';
+
+        tabs[i].classList.add('tabheader__item_active');
+
+    }
+    
+    HideTabsContent();
+    ShowTabsContent(0);
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')){
+            tabs.forEach((item, i) => {
+                if(item == target){
+                    HideTabsContent();
+                    ShowTabsContent(i);
+                }
+            });
+        }g
+
+    });
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
